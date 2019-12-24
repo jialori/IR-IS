58578809244d83346a77e8bba04d5e051f37ccf9
jialori <lori.jia@mail.utoronto.ca>
2019-12-24 12:03:50 -0700

    Initialize repo

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float movementVelocityX, movementVelocityY;
+	public float movementAccelerationX, movementAccelerationY;
+	public float jumpPower;
+
+	// private Vector3 gravityDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	Vector3 gravityDirection = new Vector3(0, -1, 0);
+    	Vector3 velocityVector;
+
+    	if (Controller.GetJump()) movementVector += new Vector3(0, jumpPower, 0);
+
+    	transform.position += velocityVector * Time.deltaTime;
+
+    	velocityVector += 9.81 * gravityDirection * Time.deltaTime;
+
+    	// Vector2 curSpeed = a * targetSpeed + (1-a) * curSpeed;
+
+		// float hMovement = Controller.GetHorizontal() * speed * Time.deltaTime;
+		// float vMovement = Controller.GetVertical() * speed * Time.deltaTime;
+
+		// transform.Translate(hMovement);
+        
+    }
+
+}
---
+++ Assets/Scripts/Util.cs
@@ -0,0 +1,126 @@
+using UnityEngine;
+
+namespace Util
+{
+	// class Util
+	// {
+	// 	public static string GetFormattedTime(float time)
+	// 	{
+	// 		var mins = Mathf.Floor(time / 60);
+	// 		var secs = time % 60;
+	// 		return string.Format("{0:00} :  {1:00} ", mins, secs);
+	// 	}
+	// }
+
+	class Controller
+	{
+		public static bool Get Jump()
+		{
+			// if (GameManager.instance.UseController)
+			// {
+			// 	return Input.GetButtonDown("A");
+			// }
+			// else
+			// {
+				return Input.GetKeyDown(KeyCode.Space);
+			// }
+		}
+
+		// public static bool GetB()
+		// {
+		// 	if (GameManager.instance.UseController)
+		// 	{
+		// 		return Input.GetButtonDown("B");
+		// 	}
+		// 	else
+		// 	{
+		// 		return Input.GetKeyDown("e");
+		// 	}
+		// }
+
+		public static float GetVertical()
+		{
+			return Input.GetAxis("Horizontal");
+			// if (GameManager.instance.UseController)
+			// {
+			// 	return Input.GetButtonDown("X");
+			// }
+			// else
+			// {
+			// 	return Input.GetKeyDown("x");
+			// }
+		}
+
+		public static float GetHorizontal()
+		{
+			return Input.GetAxis("Vertical");
+			// if (GameManager.instance.UseController)
+			// {
+			// 	return Input.GetButtonDown("Y");
+			// }
+			// else
+			// {
+			// 	return Input.GetKeyDown("y");
+			// }
+		}
+
+
+		// public static float GetXAxis()
+		// {
+		// 	if (GameManager.instance.UseController)
+		// 	{
+		// 		return Input.GetAxis("LeftJoystickX");
+		// 	}
+		// 	else
+		// 	{
+		// 		return Input.GetAxis("Horizontal");
+		// 	}
+		// }
+
+		// public static float GetYAxis()
+		// {
+		// 	if (GameManager.instance.UseController)
+		// 	{
+		// 		return -Input.GetAxis("LeftJoystickY");
+		// 	}
+		// 	else
+		// 	{
+		// 		return Input.GetAxis("Vertical");
+		// 	}
+		// }
+
+		// public static float GetCamXAxis()
+		// {
+		// 	if (GameManager.instance.UseController)
+		// 	{
+		// 		return Input.GetAxis("RightJoystickX");
+		// 	}
+		// 	else
+		// 	{
+		// 		return Input.GetAxis("Mouse X");
+		// 	}
+		// }
+
+		// public static float GetCamYAxis()
+		// {
+		// 	if (GameManager.instance.UseController)
+		// 		return Input.GetAxis("RightJoystickY");
+		// 	else
+		// 		return Input.GetAxis("Mouse Y");
+		// }
+	}
+
+	// class MyLayers
+	// {
+	// 	static private string breakableMaskName = "Breakable";
+	// 	static private string knockableMaskName = "Knockable";
+	// 	static private string toolsMaskName = "Tools";
+	// 	static private string interactableMaskName = "Interactable";
+
+	// 	static public int breakableMask = 1 << LayerMask.NameToLayer(breakableMaskName);
+	// 	static public int knockableMask = 1 << LayerMask.NameToLayer(knockableMaskName);
+	// 	static public int toolsMask = 1 << LayerMask.NameToLayer(toolsMaskName);
+	// 	static public int interactableMask = 1 << LayerMask.NameToLayer(interactableMaskName);
+	// }
+
+}
